74ba66350672d468c15841d8dac90f9a65e530dc
Mukesh-Kumar987 <mukeshrajput5095@gmail.com>
2024-09-16 12:46:07 +0530

    updation

--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,9 @@ router.get('/users', (req, res)=>{
     usercontroller.getAllUsers(req, res);
 })
 
+router.get('/user/:id', (req, res )=>{
+    usercontroller.getUser(req, res);
+})
+
 
 module.exports=router;
